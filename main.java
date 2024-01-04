f4cfbfaef0726e164e508fbd2d8065e0b9fbad5c
HuangDoing <2185145264@qq.com>
2024-01-04 12:56:33 +0800

    doing1的第er次提交

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("第二次");
-        System.out.println("doing1的第一次提交");
+        System.out.println("doing1的第er次提交");
     }
 }
